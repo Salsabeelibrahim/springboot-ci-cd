a079e96937acc7c26cdc356e5402be5f99e1a49a
Salsabeelibrahim <202202403@bethlehem.edu>
2025-06-01 15:10:40 +0300

    Add application-test.properties for RDS CI tests

--- employees/src/test/java/com/example/payroll/PayrollApplicationTests.java
+++ employees/src/test/java/com/example/payroll/PayrollApplicationTests.java
@@ -3,7 +3,8 @@ package com.example.payroll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
+@SpringBootTest(properties = "spring.profiles.active=test")
 class PayrollApplicationTests {
 
 	@Test
